e5859d0b0202514103e148c92b9cd81b56a8fd01
Nicholas McClay <nmcclay@wombatsecurity.com>
2017-07-08 20:08:58 -0400

    added faker.js mocks

--- database.js
+++ database.js
@@ -1,6 +1,24 @@
+var Post = require('./models/posts');
+var env = process.env.NODE_ENV || 'development';
+
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://localhost/mean-db');
+
+var generateMock = function(model, count, generateFake) {
+  console.log("purging all " + model.modelName + "...");
+  Post.remove({}, function(err) {
+    let mocks = [];
+    for (var i=0; i < count; i++) {
+      mocks.push(generateFake());
+    }
+
+    Post.create(mocks, function (err) {
+      if (err) return console.error('Failed to create mock ' + model.modelName);
+      console.log(count + " mock " + model.modelName + " created");
+    });
+  });
+};
 
 var mongo = mongoose.connection;
 var db = mongo.db;
@@ -10,6 +28,17 @@ mongo.once('open', function() {
   db.collection('posts').count().then(function(count) {
     console.log("post count: " + count);
   });
+
+  if (env == 'development') {
+    var faker = require('faker');
+    generateMock(Post, 10, function() {
+      return {
+        title: faker.lorem.words(),
+        content: faker.lorem.sentences(),
+        published: faker.date.past()
+      }
+    });
+  }
 });
 
 module.exports = db;
